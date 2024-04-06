b85f9202298893b7870a882c44f9e711482555b7
Shailendra Singh <shailendra.singh.knp@gmail.com>
2024-04-06 13:54:59 +0530

    test PR

--- train.py
+++ train.py
@@ -9,7 +9,14 @@ y = df['Disease'].to_numpy()
 labels = np.sort(np.unique(y))
 y = np.array([np.where(labels == x) for x in y]).flatten()
 
-model = LogisticRegression().fit(X, y)
+model = LogisticRegression(
+    # penalty='l2',          # Regularization type (l2: Ridge regularization)
+    # C=1.0,                 # Inverse of regularization strength (lower values specify stronger regularization)
+    # solver='lbfgs',        # Algorithm to use in the optimization problem (lbfgs: Limited-memory Broyden–Fletcher–Goldfarb–Shanno algorithm)
+    # max_iter=100,          # Maximum number of iterations for optimization
+    random_state=42        # Random seed for reproducibility
+    
+).fit(X, y)
 
 with open("model.pkl", 'wb') as f:
     pickle.dump(model, f)
